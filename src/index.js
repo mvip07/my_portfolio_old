19f91b777ffda05b201e17420e45b5a2876ef9ee
mirabzal-07 <mirabzalozodov07@gmail.com>
2022-12-04 17:17:27 +0500

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from "./App";
+import { createRoot } from 'react-dom/client';
 
 const container = document.getElementById('main');
 const root = createRoot(container);
