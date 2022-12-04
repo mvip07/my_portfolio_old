f2eaa514a31ffda820c565994304fafe2b9977a3
mirabzal-07 <mirabzalozodov07@gmail.com>
2022-12-04 17:02:07 +0500

    update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import * as Sentry from "@sentry/react";
 import App from "./App";
 
-Sentry.init({
-  dsn: "https://localhost@sentry.io/3000"
-});
 const container = document.getElementById('main');
 const root = createRoot(container);
 root.render(<BrowserRouter><App /></BrowserRouter>);
