8da535f3c0112fb606e18687adcc956f7430e238
mirabzal-07 <mirabzalozodov07@gmail.com>
2022-12-04 16:58:29 +0500

    error fixed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,6 @@
-import { useState } from "react"
-
 function ToggleBtn() {
     const hours = new Date().getHours()
-    const [btn, setBtn] = useState(hours <= 12 ? true : false)
+    const btn  = hours <= 12 ? true : false
     const body = document.querySelector("body")
     body.classList.toggle(`${btn === false ? "dark" : "light"}`)
     return (
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from "./App"
+import * as Sentry from "@sentry/react";
+import App from "./App";
+
+Sentry.init({
+  dsn: "https://localhost@sentry.io/3000"
+});
 const container = document.getElementById('main');
 const root = createRoot(container);
 root.render(<BrowserRouter><App /></BrowserRouter>);
